f87e7eedf2da82fc48a16665d914c467952b9426
[jeerawuth] <[jeerawuth@me.com]>
2019-01-23 16:09:49 +0700

    menu-login-logout

--- src/app/auth/auth.service.ts
+++ src/app/auth/auth.service.ts
@@ -1,18 +1,23 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Subject } from 'rxjs';
+
 import { AuthData } from './auth-data.model';
 
 @Injectable({ providedIn: 'root'})
 export class AuthService {
 
   private token: string;
-
+  private authStatusListener = new Subject<boolean>();
   constructor(private http: HttpClient) {}
 
   getToken() {
     return this.token;
   }
 
+  getAuthStatusListener() {
+    return this.authStatusListener.asObservable();
+  }
   createUser(email: string, password: string) {
     const authData: AuthData = { email: email, password: password };
     this.http.post('http://localhost:3000/api/user/signup', authData)
@@ -28,6 +33,7 @@ export class AuthService {
       // console.log(response);
       const token = response.token;
       this.token = token;
+      this.authStatusListener.next(true);
     });
   }
 
--- src/app/header/header.component.ts
+++ src/app/header/header.component.ts
@@ -1,16 +1,34 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, OnDestroy } from '@angular/core';
 import { FormGroup, FormControl } from '@angular/forms';
+import { Subscription } from 'rxjs';
+
+import { AuthService } from '../auth/auth.service';
 
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
   styleUrls: ['./header.component.css']
 })
-export class HeaderComponent implements OnInit {
+export class HeaderComponent implements OnInit, OnDestroy {
   searchForm: FormGroup;
+  userIsAuthenticated = false;
+  private authListenerSubs: Subscription;
+  constructor(private authService: AuthService) {}
   ngOnInit() {
     this.searchForm = new FormGroup({
       'search': new FormControl(null)
     });
+    this.authListenerSubs = this.authService
+    .getAuthStatusListener()
+    .subscribe(isAuthenticated => {
+      this.userIsAuthenticated = isAuthenticated;
+    });
+
+  }
+  ngOnDestroy() {
+    this.authListenerSubs.unsubscribe();
+  }
+  onLogout() {
+    // logout here
   }
 }
